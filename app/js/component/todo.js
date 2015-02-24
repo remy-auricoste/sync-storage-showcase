c47bc5c658ec737f07f687e32efb932c2cd22332
remy <remy.auricoste@gmail.com>
2015-02-24 23:37:40 +0100

    fix edit

--- app/js/component/todo.js
+++ app/js/component/todo.js
@@ -5,7 +5,6 @@ directives.addTemplate("todo", {
 }, function($scope, attr, elem) {
     $scope.onclick = function() {
         $scope.edited = true;
-        console.log(elem);
     }
     $scope.onblur = function() {
         $scope.edited = false;
@@ -17,50 +16,3 @@ directives.addTemplate("todo", {
         $scope.todo.done = !$scope.todo.done;
     }
 });
-
-
-var TodoView = function(todo) {
-        var data = _.extend({}, todo);
-        data = _.extend(data, {
-            edited: false,
-            binds: {
-                ".todo_title": {
-                    onclick: function() {
-                        data.done = !data.done;
-                        saveTodo(data);
-                        data.render();
-                    },
-                    ondblclick: function() {
-                        data.edited = true;
-                        data.render();
-                        var element = S(".todo_edit", S("#" + data._templateId), true);
-                        element.focus();
-                    }
-                },
-                ".todo_checkbox": {
-                    onclick: function() {
-                        data.done = !data.done;
-                        saveTodo(data);
-                        data.render();
-                    },
-                },
-                ".todo_edit": {
-                    onblur: function() {
-                        data.edited = false;
-                        data.title = this.value;
-                        saveTodo(data);
-                        data.render();
-                    }
-                },
-                ".todo_delete": {
-                    onclick: function() {
-                        data.deleted = true;
-                        saveTodo(data);
-                        data.render();
-                    }
-                }
-            }
-        });
-        return data;
-
-    }
